e39ffae28e3ca84d5892bece850abdeef05ad431
Juliusz Kuprel <juliusz_k@exeno.com>
2023-09-28 23:29:38 +0200

    Added controller and basic configuration

---
+++ src/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MainController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("Siemanko");
+    }
+}
